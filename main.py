950a2c7d7d5483dd03002fe01b9625d828ef7220
Malak Ettabia <malakettabia00@gmail.com>
2025-10-21 18:36:53 +0200

    create the player

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+turtle = Player()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    screen.listen()
+    screen.onkey(turtle.go_up, "Up")
     screen.update()
